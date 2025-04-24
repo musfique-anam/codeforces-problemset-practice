4435ccd94d9cd9df1e298a8c6959eed1615c0dab
Musfique Anam <musfique2002.personal@gmail.com>
2025-04-24 22:14:20 +0600

    Solved

---
+++ A_Way Too Long Words.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str,a,b;
+    int t,A,N;
+    cin>>t;
+    while(t--){
+        cin>>str;
+        A=str.size();
+        a=str[0];
+        b=str[A-1];
+        if(A>=12){
+            N=A-2;
+            cout<<a<<N<<b<<endl;
+        }else{
+            cout<<str<<endl;
+        }
+    }
+}
