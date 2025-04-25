1c0a2f30c0eef7b0f4e4bd5d771e0e865d8d662c
Musfique Anam <musfique2002.personal@gmail.com>
2025-04-25 19:52:12 +0600

    Accepted

--- A_Elephant.cpp
+++ A_Elephant.cpp
@@ -8,22 +8,22 @@ int main(){
         Steps=(Distance/5);
         Distance=(Distance % Steps);
     }
-//     if(Distance <=4){
-//         Steps=(Distance/4);
-//         Distance=(Distance % Steps);
-//     }
-//     if(Distance <=3){
-//         Steps=(Distance/3);
-//         Distance=(Distance % Steps);
-//     }
-//     if(Distance <=2){
-//         Steps=(Distance/2);
-//         Distance=(Distance % Steps);
-//     }
-//     if(Distance <=1){
-//         Steps=(Distance/1);
-//         Distance=(Distance % Steps);
-//     }
+    if(Distance <=4){
+        Steps=(Distance/4);
+        Distance=(Distance % Steps);
+    }
+    if(Distance <=3){
+        Steps=(Distance/3);
+        Distance=(Distance % Steps);
+    }
+    if(Distance <=2){
+        Steps=(Distance/2);
+        Distance=(Distance % Steps);
+    }
+    if(Distance <=1){
+        Steps=(Distance/1);
+        Distance=(Distance % Steps);
+    }
 
-//     cout<<Steps;
+    cout<<Steps;
 }
--- A_Stones on the Table.cpp
+++ A_Stones on the Table.cpp
@@ -2,21 +2,14 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int a,b,t,size,count=0;
+    int t,count=0;
     string str;
-    while(t--){
-        cin>>str;
-        for(int i=0;i<=str.length();i++){
-            a=str[i];
-            b=str[i+1];
-            cout <<"AB"<<a<<" "<<b<<endl;
-            if(str[i]==str[i+1]){
-                count++;
-            }
+    cin>>t;
+    cin>>str;
+    for(int i=0;i<t;i++){
+        if(str[i]==str[i+1]){
+            count++;
         }
     }
-    // cout<<count;
+    cout<<count;
 }
