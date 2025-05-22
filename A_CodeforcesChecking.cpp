40ae3fea66289714e5f4c14f984632879ead137a
Musfique Anam <musfique2002.personal@gmail.com>
2025-05-22 22:23:16 +0600

    Accepted

---
+++ A_CodeforcesChecking.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string c;
+        cin>>c;
+        if(c=="c" || c=="o" || c=="d" || c=="e" || c=="f" || c=="r" || c=="e" || c=="s"){
+            cout<<"YES"<<endl;
+        }else{{
+            cout<<"NO"<<endl;
+        }}
+    }
+}
