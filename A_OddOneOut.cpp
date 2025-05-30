2b1ad5983a9d0a36405ff93c33e0b0dad7016174
Musfique Anam <musfique2002.personal@gmail.com>
2025-05-30 20:15:21 +0600

    Accepted

---
+++ A_OddOneOut.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,a,b,c;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c;
+        if(a==b){
+            cout<<c<<endl;
+        }else if(b==c){
+            cout<<a<<endl;
+        }else if(c==a){
+            cout<<b<<endl;
+        }
+    }
+}
