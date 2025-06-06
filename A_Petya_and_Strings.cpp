f3122095b08d892d36d2296a0742c4441b9e18af
Musfique Anam <musfique2002.personal@gmail.com>
2025-06-07 00:05:48 +0600

    fails hidden test cases

--- A_Petya_and_Strings.cpp
+++ A_Petya_and_Strings.cpp
@@ -1,13 +1,18 @@
-//Trying
-
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int z;
     string str1,str2;
     cin>>str1>>str2;
-    int z=lexicographical_compare(str1, str1.size(), str2, str2.size());
-    if(z==1){
-
+    int n=str1.size();
+    for(int i=0;i<n;i++){
+        if(tolower(str1[i] == tolower(str2[i]))){
+            z=0;
+        }else if(tolower(str1[i] < tolower(str2[i]))){
+            z=-1;
+        }else if(tolower(str1[i] > tolower(str2[i]))){
+            z=1;
+        }
     }
-    cout<<z;
+    cout<<z<<endl;
 }
