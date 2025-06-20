15771a49a837da59ea0bc186b9a4d0e7cab1ec03
Musfique Anam <musfique2002.personal@gmail.com>
2025-06-20 17:51:28 +0600

    WA

---
+++ A_Hulk.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    string s1="I hate";
+    string s2="I love ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<s1;
+    }    
+    cout<<" it"<<endl;
+}
