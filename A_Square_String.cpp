7dc82815d35ee8be03f07a7c303a7ed4847cf774
Musfique Anam <musfique2002.personal@gmail.com>
2025-07-11 19:52:12 +0600

    WA

---
+++ A_Square_String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        for(int i=0;i<str.length();i++){
+
+        }
+    }
+}
