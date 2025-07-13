22caa21a4520319d6fdc4c6da122e8a840c73d01
Musfique Anam <musfique2002.personal@gmail.com>
2025-07-14 03:16:09 +0600

    WA

---
+++ B_Increasing.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n,a;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            cin>>a;
+            v.push_back(a);
+        }
+    }
+}
