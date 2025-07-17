f11229b0e0df256d8ae56b660acf4f333976a841
Musfique Anam <musfique2002.personal@gmail.com>
2025-07-17 09:33:42 +0600

    Accepted

---
+++ A_Vus_the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A,P,N;
+    cin>>A>>P>>N;
+
+    if(P<A || N<A){
+        cout<<"No"<<endl;
+    }else{
+        cout<<"Yes"<<endl;
+    }
+}
