8ea883e1d4507c3262afb6217243de9686a7dd1f
Musfique Anam <musfique2002.personal@gmail.com>
2025-07-25 22:07:59 +0600

    not solved

---
+++ A_Trippi_Troppi.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    string str;
+    while(t--){
+        cin>>str;
+        for(int i=0;i<str.size();i++){
+            if(str[i] = ' '){
+                
+            }
+        }
+
+
+        
+    }
+}
