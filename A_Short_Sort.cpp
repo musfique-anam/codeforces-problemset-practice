5ebabbfd2f2bac2d9c10e9c29ea505b5f3d3c196
Musfique Anam <musfique2002.personal@gmail.com>
2025-08-03 18:38:38 +0600

    Accepted

--- A_Short_Sort.cpp
+++ A_Short_Sort.cpp
@@ -8,7 +8,29 @@ int main(){
     while(t--){
         cin>>str;
 
-
-        
+        if(str == "abc"){
+            cout<<"YES"<<endl;
+        }else{
+            swap(str[0],str[1]);
+            if(str == "abc"){
+                cout<<"YES"<<endl;
+            }else{
+                swap(str[0],str[1]);
+                swap(str[1],str[2]);
+                
+                if(str == "abc"){
+                    cout<<"YES"<<endl;
+                }else{
+                    swap(str[1],str[2]);
+                    swap(str[0],str[2]);
+            
+                    if(str == "abc"){
+                        cout<<"YES"<<endl;
+                    }else{
+                        cout<<"NO"<<endl;
+                    }
+                }
+            }
+        }
     }
 }
--- A_Trippi_Troppi.cpp
+++ A_Trippi_Troppi.cpp
@@ -17,3 +17,34 @@ int main(){
         
     }
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string trippi(string str){
+    string ans="";
+
+    bool a=true;
+    for(int i=0; i<str.length();i++){
+        if(str[i] == ' '){
+            a=true;
+        }else if(str[i] != ' ' && a==true){
+            ans.push_back(str[i]);
+            a=false;
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        string str;
+        cin>>str;
+
+        cout<<trippi(str)<<endl;
+    }
+}
