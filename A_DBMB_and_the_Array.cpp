a3bf82bb798551be384e6766ae2973d1db97f7b9
musfique-anam <m.musfique2002@gmail.com>
2026-01-25 21:48:53 +0600

    Accepted

---
+++ A_DBMB_and_the_Array.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fun(int sum,int s,int x){
+    sum+=x;
+    if(sum == s){
+        return -1;
+    }else{
+        fun(sum,s,x);
+        if(sum > s){
+            return 5;
+        }
+    }
+
+}
+
+int main(){
+    int n,s,x,a,sum=0,t;
+    vector<int> v;
+
+    cin>>t;
+
+    while(t--){
+
+        cin>>n>>s>>x;
+    
+        for(int i=0;i<n;i++){
+            cin>>a;
+            v.push_back(a);
+            sum+=a;
+        }
+    
+        if(sum == s)
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(sum > s)
+        {
+            cout<<"NO"<<endl;
+        }
+        else if(sum < s)
+        {
+            while(sum<s){
+                sum+=x;
+                if(sum == s){
+                    cout<<"YES"<<endl;
+                    break;
+                }
+            }
+            if(sum>s){
+                cout<<"NO"<<endl;
+            }
+        }
+        sum=0;
+        v.clear();
+    }
+}
